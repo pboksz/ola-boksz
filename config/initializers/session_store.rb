af20ec24f589d4a3e630bd99a10da3ef6d28d592
Phillip Boksz <pboksz@gmail.com>
2023-09-04 14:40:43 +0100

    Update last few usages of old app name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hania-boksz_session'
+Rails.application.config.session_store :cookie_store, key: '_ola-boksz_session'
